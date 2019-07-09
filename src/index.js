3db7bd1f3992605a6d60c4ab473fef39ef529a65
David <david.shams@gmail.com>
2019-07-08 23:33:06 -0500

    browsersync babel and hot-reload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import bar from './module';
-import App from './components/Home.jsx';
+// import bar from './module';
+// import App from './components/Home.jsx';
 
-document.write(bar());
+// document.write(bar());
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,74 @@
 const path = require('path');
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    /** 
-     * Old webpack
-     */
-    // module: {
-    //     loaders: {
-    //         test: /.js$/,
-    //         exclude: /node_modules/,
-    //         loader: 'babel-loader'
-    //     }
-    // }
-    /**
-     * New webpack
-     * see https://webpack.js.org/loaders/babel-loader/
-     */
-    // module: {
-    //     rules: [
-    //       {
-    //         test: /\.m?js$/,
-    //         exclude: /(node_modules|bower_components)/,
-    //         use: {
-    //           loader: 'babel-loader',
-    //           options: {
-    //             presets: ['@babel/preset-env']
-    //           }
-    //         }
-    //       }
-    //     ]
-    //   }
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true,
 
-      module: {
-        rules: [
-          { test: /\.js$/, use: 'babel-loader' },
-          { test: /\.jsx$/, use: 'babel-loader' }
-        ]
-      }
-      
-}
+  },
+
+  /** 
+   * Old webpack
+   */
+  // module: {
+  //     loaders: {
+  //         test: /.js$/,
+  //         exclude: /node_modules/,
+  //         loader: 'babel-loader'
+  //     }
+  // }
+  /**
+   * New webpack
+   * see https://webpack.js.org/loaders/babel-loader/
+   */
+  // module: {
+  //     rules: [
+  //       {
+  //         test: /\.m?js$/,
+  //         exclude: /(node_modules|bower_components)/,
+  //         use: {
+  //           loader: 'babel-loader',
+  //           options: {
+  //             presets: ['@babel/preset-env']
+  //           }
+  //         }
+  //       }
+  //     ]
+  //   }
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        // options: { preset: ['env'] }
+
+      },
+      // { test: /\.js$/, use: 'babel-loader' },
+      // { test: /\.jsx$/, use: 'babel-loader' }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 8080,
+      proxy: 'http://localhost:3000/',
+    }),
+  ]
+};
